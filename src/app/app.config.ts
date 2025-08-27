615904c4a4cca505073c6aad58038d9a5e080b9e
Joel Yovera <ingyovera6114@gmail.com>
2025-08-27 16:52:13 -0500

    Integración de FontAwesome y mejoras

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,21 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
+
+export function provideFontAwesome(library: FaIconLibrary) {
+  library.addIconPacks(fas, far, fal);
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: FaIconLibrary, useFactory: () => new FaIconLibrary() },
+    { provide: FontAwesomeModule, useFactory: () => provideFontAwesome }
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule, FaIconLibrary } from "@fortawesome/angular-fontawesome";
+import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fal } from '@fortawesome/pro-light-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FontAwesomeModule
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('ejercicios-part2.0');
+  
+  constructor(private library: FaIconLibrary) {
+    // Registrar todos los iconos de cada paquete
+    library.addIconPacks(fas, far, fal);
+  }
 }
